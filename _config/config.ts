32083f65834fc9c168885ce9c3bd16c02688ff4f
potakusan <msqkn310@gmail.com>
2024-08-06 19:16:56 +0900

    Update config.ts

--- _config/config.ts
+++ _config/config.ts
@@ -1,6 +1,6 @@
 export const db_setting = {
   host: process.env.DB_HOST,
-  user: process.env.DB_ADMIN,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   dateStrings: true,
